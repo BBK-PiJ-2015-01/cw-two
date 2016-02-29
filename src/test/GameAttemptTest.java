c89191856c3095a2d49c18bb9b9784f8a8ba019e
sbaird02 <sbaird02@CSPC030.dcs.bbk.ac.uk>
2016-02-29 13:07:55 +0000

    More GameAttempt tests

--- src/test/GameAttemptTest.java
+++ src/test/GameAttemptTest.java
@@ -9,22 +9,27 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 
+import mastermind.AttemptResult;
+import mastermind.AttemptResultImpl;
 import mastermind.GameAttempt;
 import mastermind.GameAttemptImpl;
 
 public class GameAttemptTest {
 
 	private GameAttempt instance;
-	
+
 	@Before
 	public void setUp() {
-		
+
 		instance = new GameAttemptImpl();
 	}
 
+	// *************************** Attempt list tests
+	// *********************************
+
 	@Test
 	public void getAttempListNotSetTest() {
-		
+
 		List<?> expectedAttemptList = null;
 		List<?> resultAttemptList = instance.getAttempt();
 		assertEquals(expectedAttemptList, resultAttemptList);
@@ -32,22 +37,46 @@ public class GameAttemptTest {
 
 	@Test
 	public void setAttempListWithEmptyListTest() {
-		
+
 		instance.setAttempt(Collections.EMPTY_LIST);
-	}	
-	
-	@Test(expected=IllegalArgumentException.class)
+	}
+
+	@Test(expected = IllegalArgumentException.class)
 	public void setAttempListWithNullListTest() {
-		
+
 		instance.setAttempt(null);
 	}
-	
+
 	@Test
 	public void getAttempListAfterValidSetterTest() {
-		
-		List<?> expectedAttemptList = Arrays.asList(new String[] {"Any string value here"});
+
+		List<?> expectedAttemptList = Arrays.asList(new String[] { "Any string value here" });
 		instance.setAttempt(expectedAttemptList);
 		List<?> resultAttemptList = instance.getAttempt();
 		assertEquals(expectedAttemptList, resultAttemptList);
 	}
+
+	// *************************** GameResult tests
+	// *********************************
+
+	@Test
+	public void getAttemptResultNotSetTest() {
+
+		AttemptResult expectedAttempResult = null;
+		AttemptResult resultAttempResult = instance.getAttemptResult();
+		assertEquals(expectedAttempResult, resultAttempResult);
+	}
+
+	@Test
+	public void setAttempResultWithEmptyResultTest() {
+
+		instance.setAttemptResult(new AttemptResultImpl());
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void setAttempResultWithNullTest() {
+
+		instance.setAttemptResult(null);
+	}
+
 }
