23806766f139f2110c8eaaea8ee523e2421a13e8
sbaird02 <sbaird02@CSPC030.dcs.bbk.ac.uk>
2016-02-29 11:45:35 +0000

    Created AtttemptResult

--- src/mastermind/AttemptResult.java
+++ src/mastermind/AttemptResult.java
@@ -1,5 +1,34 @@
 package mastermind;
 
+/**
+ * Class to encapsulate the results of evaluating an attempt to guess the secret code
+ * @author sbaird02
+ *
+ */
+		
 public interface AttemptResult {
 
+	/**
+	 * 
+	 * @return the number of 'pegs' which are in the correct position
+	 */
+	Long getExactScore();
+
+	void setExactScore(Long score);
+
+	/**
+	 * 
+	 * @return the number of 'pegs' of the correct colour but in an incorrect position
+	 */
+	Long getNearScore();
+
+	void setNearScore(Long score);
+
+	/**
+	 * 
+	 * @return a String containing a message pertaining to the attempt
+	 */
+	String getMessage();
+
+	void setMessage(String message);
 }
