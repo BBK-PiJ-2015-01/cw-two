b7c9533c942d380deb864bb72c41d961f0799dd4
sbaird02 <sbaird02@CSPC030.dcs.bbk.ac.uk>
2016-02-29 14:36:05 +0000

    game solution tests

--- src/test/GameStateTest.java
+++ src/test/GameStateTest.java
@@ -1,25 +1,87 @@
 package test;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import mastermind.GameState;
 import mastermind.GameStateImpl;
+import mastermind.GameStatus;
 
 public class GameStateTest {
 
 	private GameState instance;
+
 	@Before
 	public void setUp() throws Exception {
-		
+
 		instance = new GameStateImpl();
+	}
+	// ************************ Solution tests *******************************
+
+	@Test
+	public void getGameSolutionTest() {
+
+		List<?> expectedGameSolution = null;
+		List<?> resultGameSolution = instance.getSolution();
+		assertEquals(expectedGameSolution, resultGameSolution);
+	}
+	
+	@Test
+	public void setGameSolutionTest() {
+		
+		List<String> expectedSolution = Arrays.asList(new String[] {"Any string value here"});
+		instance.setSolution(expectedSolution);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void setGameSolutionWithNullTest() {
+
+		instance.setSolution(null);
+	}
+	
+	// ************************ Message tests *******************************
+	@Test
+	public void getGameStatus() {
+
+		GameStatus expectedGameStatus = null;
+		GameStatus resultGameStatus = instance.getGameStatus();
+		assertEquals(expectedGameStatus, resultGameStatus);
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void setGameStatusWithValidValueTest() {
+
+		instance.setGameStatus(GameStatus.IN_PLAY);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void setGameStatusWithNullTest() {
+
+		instance.setGameStatus(null);
+	}
+
+	// ************************ Message tests *******************************
+
+	@Test
+	public void getMessageNotSetTest() {
+
+		String expectedMessage = null;
+		String resultMessage = instance.getMessage();
+		assertEquals(expectedMessage, resultMessage);
+	}
+
+	@Test
+	public void getMessageAfterValidSetterTest() {
+
+		String expectedMessage = "expectedMessage";
+		instance.setMessage(expectedMessage);
+		String resultMessage = instance.getMessage();
+		assertEquals(expectedMessage, resultMessage);
 	}
 
 }
