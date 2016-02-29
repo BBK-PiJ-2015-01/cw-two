468baf265ecd39d7c08e10e9a46155a0d634f5b9
sbaird02 <sbaird02@CSPC030.dcs.bbk.ac.uk>
2016-02-29 13:47:40 +0000

    Tests for number of permutations

---
+++ src/test/GameDetailsTest.java
@@ -0,0 +1,91 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import mastermind.AttemptResultImpl;
+import mastermind.GameDetails;
+import mastermind.GameDetailsImpl;
+
+public class GameDetailsTest {
+
+	private GameDetails instance;
+
+	@Before
+	public void setUp() throws Exception {
+
+		instance = new GameDetailsImpl();
+	}
+
+	// ************************ Attempt# tests *******************************
+
+	@Test
+	public void getNumOfAttemptsNotSetTest() {
+
+		Long expectedNumOfAttempts = null;
+		Long resultNumOfAttempts = instance.getNumberOfAttempts();
+
+		assertEquals(expectedNumOfAttempts, resultNumOfAttempts);
+	}
+
+	@Test
+	public void setNumOfAttemptsValidValueTest() {
+
+		instance.setNumberOfAttempts(1L);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void setNumOfAttemptsNullTest() {
+
+		instance.setNumberOfAttempts(null);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void setNumOfAttemptsTONegativeNumberTest() {
+
+		instance.setNumberOfAttempts(-1L);
+	}
+	
+	@Test
+	public void getNumOfAttemptsAfterValidSetterTest() {
+
+
+		Long expectedNumOfAttempts = 12L;
+		instance.setNumberOfAttempts(expectedNumOfAttempts);
+		
+		Long resultNumOfAttempts = instance.getNumberOfAttempts();
+		assertEquals(expectedNumOfAttempts, resultNumOfAttempts);
+	}
+
+	// ************************ Permutations# tests *******************************
+	
+	@Test
+	public void getNumOfPermutationsNotSetTest() {
+
+		Long expectedNumOfPermutations = null;
+		Long resultNumOfPermutations = instance.getNumberOfPermutations();
+
+		assertEquals(expectedNumOfPermutations, resultNumOfPermutations);
+	}
+	
+	@Test
+	public void setNumOfPermutationsValidValueTest() {
+
+		instance.setNumberOfPermutations(1L);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void setNumOfPermutationsNullTest() {
+
+		instance.setNumberOfPermutations(null);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void setNumOfPermutationsToNegativeNumberTest() {
+
+		instance.setNumberOfPermutations(-1L);
+	}
+}
