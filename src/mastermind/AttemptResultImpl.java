c34636656f34e7cbc634b6245d220b9b33b33cfb
sbaird02 <sbaird02@CSPC030.dcs.bbk.ac.uk>
2016-02-29 12:35:57 +0000

    Some javadoc

--- src/mastermind/AttemptResultImpl.java
+++ src/mastermind/AttemptResultImpl.java
@@ -9,14 +9,25 @@ public class AttemptResultImpl implements AttemptResult {
 	private final String NEGATIVE_VALUE_MSG = "A negative value is not allowed";
 	private final String NULL_VALUE_MSG = "A null value is not allowed";
 
-	public AttemptResultImpl(){
-		
+	public AttemptResultImpl() {
+
 	}
+
+	/**
+	 * Convenience constructor to generate with scores
+	 * 
+	 * @param exactScore
+	 *            the correct guesses
+	 * @param nearScore
+	 *            the near guesses
+	 * @throws IllegalArgumentException
+	 *             if the either score is null or negative
+	 */
 	public AttemptResultImpl(Long exactScore, Long nearScore) {
 
 		init(exactScore, nearScore);
 	}
-	
+
 	private void init(Long exactScore, Long nearScore) {
 		setExactScore(exactScore);
 		setNearScore(nearScore);
@@ -31,8 +42,10 @@ public class AttemptResultImpl implements AttemptResult {
 	/**
 	 * Set the score representing the correct colour and position guesses
 	 * 
-	 * @param score the number of correct guesses
-	 * @throws IllegalArgumentException if the guess is null or negative
+	 * @param score
+	 *            the number of correct guesses
+	 * @throws IllegalArgumentException
+	 *             if the guess is null or negative
 	 */
 	@Override
 	public void setExactScore(Long score) {
@@ -53,10 +66,13 @@ public class AttemptResultImpl implements AttemptResult {
 	}
 
 	/**
-	 * Set the score representing the correct colour but incorrect position guesses
+	 * Set the score representing the correct colour but incorrect position
+	 * guesses
 	 * 
-	 * @param score the number of correct guesses
-	 * @throws IllegalArgumentException if the guess is null or negative 
+	 * @param score
+	 *            the number of correct guesses
+	 * @throws IllegalArgumentException
+	 *             if the guess is null or negative
 	 */
 	@Override
 	public void setNearScore(Long score) {
