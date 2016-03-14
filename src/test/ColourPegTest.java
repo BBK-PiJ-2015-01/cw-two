27f2a48c23bb215abc3fa43b1266e2a57766767a
sbaird02 <sbaird02@CSPC030.dcs.bbk.ac.uk>
2016-03-14 11:51:18 +0000

    Tokeniser tests

--- src/test/ColourPegTest.java
+++ src/test/ColourPegTest.java
@@ -1,12 +1,16 @@
 package test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import mastermind.Colour;
 import mastermind.ColourPeg;
+import mastermind.Tokeniser;
 
 public class ColourPegTest {
 
@@ -26,4 +30,37 @@ public class ColourPegTest {
 		new ColourPeg("");
 	}
 
+	@Test
+	public void colourIsSetTest() {
+
+		String expectedColour = "Green";
+		Colour instance = new ColourPeg(expectedColour);
+		String resultColour = instance.getColour();
+		assertEquals(expectedColour, resultColour );
+		
+	}
+	
+	@Test
+	public void tokenIsValidTest() {
+
+		Character expectedToken = 'G';
+		Tokeniser instance = new ColourPeg("Green");
+		Character resultToken = instance.getToken();
+		assertEquals(expectedToken, resultToken );
+	}
+	
+	@Test
+	public void tokenIsUniqueTest() {
+
+		Tokeniser blue = new ColourPeg("blue");
+		Tokeniser brown = new ColourPeg("brown");
+		
+		Set<Tokeniser> set = new HashSet<>();
+		
+		set.add(blue);
+		set.add(brown);
+		
+		int expectedSize= 1;
+		assertEquals(set.size(), expectedSize );
+	}
 }
